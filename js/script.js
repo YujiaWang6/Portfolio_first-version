d02657426cf91ef8a39ea3a7abed09df33facf40
YujiaWang6 <wang.yujia111@gmail.com>
2023-03-31 02:20:43 -0400

    added media queries & js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.onload = pageLoad;
+
+function pageLoad(){
+
+    var scrollbtn = document.getElementById("scrolltotop");
+
+    scrollbtn.onclick = scroll;
+
+
+    function scroll(){
+
+        window.scrollTo(0,0);
+
+    }
+
+}
+
+function toggleMenu(){
+    let menu=document.querySelector(".nav");
+    menu.classList.toggle("show-small");
+}
